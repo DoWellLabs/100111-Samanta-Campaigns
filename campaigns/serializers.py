65e1b16471676e83d314f4ebc705b47cea0efd2a
hacker777-cris <crispusgikonyo@gmail.com>
2024-03-15 14:40:44 +0300

    Getting campaigns directly from the collection

--- campaigns/serializers.py
+++ campaigns/serializers.py
@@ -29,43 +29,43 @@ class CampaignSerializer(serializers.Serializer):
     default_message = serializers.BooleanField(default=False, required=False)
 
 
-    def to_representation(self, campaign):
-        return campaign.data
+    # def to_representation(self, campaign):
+    #     return campaign.data
     
     
-    def create(self, validated_data):
-        dowell_api_key = self.context.get("dowell_api_key", None)
-        creator = self.context.get("creator", None)
-        if not creator:
-            raise serializers.ValidationError("serializer context must contain 'creator'")
-        if not isinstance(creator, DowellUser):
-            raise serializers.ValidationError("Invalid type for creator")
+    # def create(self, validated_data):
+    #     dowell_api_key = self.context.get("dowell_api_key", None)
+    #     creator = self.context.get("creator", None)
+    #     if not creator:
+    #         raise serializers.ValidationError("serializer context must contain 'creator'")
+    #     if not isinstance(creator, DowellUser):
+    #         raise serializers.ValidationError("Invalid type for creator")
         
-        audiences = validated_data.pop("audiences", [])
-        leads_links = validated_data.pop("leads_links", [])
-        validated_data["creator_id"] = creator.workspace_id
-        campaign: Campaign = Campaign.manager.create(save=False, **validated_data)
+    #     audiences = validated_data.pop("audiences", [])
+    #     leads_links = validated_data.pop("leads_links", [])
+    #     validated_data["creator_id"] = creator.workspace_id
+    #     campaign: Campaign = Campaign.manager.create(save=False, **validated_data)
 
-        for audience in audiences:
-            campaign.add_audience(audience)
-        for link in leads_links:
-            campaign.add_leads_link(link)
+    #     for audience in audiences:
+    #         campaign.add_audience(audience)
+    #     for link in leads_links:
+    #         campaign.add_leads_link(link)
             
-        campaign.save(dowell_api_key=dowell_api_key)
-        return campaign
+    #     campaign.save(dowell_api_key=dowell_api_key)
+    #     return campaign
 
     
-    def update(self, campaign: Campaign, validated_data):
-        dowell_api_key = self.context.get("dowell_api_key", None)
-        validated_data.pop("broadcast_type", None)
-        validated_data.pop("audiences", None)
-        validated_data.pop("leads_links", None)
+    # def update(self, campaign: Campaign, validated_data):
+    #     dowell_api_key = self.context.get("dowell_api_key", None)
+    #     validated_data.pop("broadcast_type", None)
+    #     validated_data.pop("audiences", None)
+    #     validated_data.pop("leads_links", None)
 
-        for attr, value in validated_data.items():
-            setattr(campaign, attr, value)
+    #     for attr, value in validated_data.items():
+    #         setattr(campaign, attr, value)
 
-        campaign.save(dowell_api_key=dowell_api_key)
-        return campaign
+    #     campaign.save(dowell_api_key=dowell_api_key)
+    #     return campaign
 
 
 
@@ -78,51 +78,51 @@ class CampaignMessageSerializer(serializers.Serializer):
     is_html_email = serializers.BooleanField(default=False, required=False)
     html_email_link = serializers.URLField(required=False, allow_null=True)
 
-    def to_representation(self, campaign_message):
-        return campaign_message.data
+    # def to_representation(self, campaign_message):
+    #     return campaign_message.data
     
     
-    def create(self, validated_data):
-        campaign = self.context.get("campaign", None)
-        dowell_api_key = self.context.get("dowell_api_key", None)
-        if not campaign:
-            raise exceptions.ValidationError("serializer context must contain 'campaign'")
-        if not isinstance(campaign, Campaign):
-            raise exceptions.ValidationError("Invalid type for campaign")
-        if campaign.get_message(dowell_api_key=settings.PROJECT_API_KEY):
-            raise exceptions.ValidationError(f"Campaign '{campaign.title}', already has a message")
+    # def create(self, validated_data):
+    #     campaign = self.context.get("campaign", None)
+    #     dowell_api_key = self.context.get("dowell_api_key", None)
+    #     if not campaign:
+    #         raise exceptions.ValidationError("serializer context must contain 'campaign'")
+    #     if not isinstance(campaign, Campaign):
+    #         raise exceptions.ValidationError("Invalid type for campaign")
+    #     if campaign.get_message(dowell_api_key=settings.PROJECT_API_KEY):
+    #         raise exceptions.ValidationError(f"Campaign '{campaign.title}', already has a message")
         
-        validated_data["type"] = campaign.broadcast_type
-        validated_data["campaign_id"] = campaign.pkey
-        # print("validated data is", validated_data)
-        sender = validated_data.get("sender", None)
-        if campaign.broadcast_type.lower() == "email":
-            if not sender:
-                validated_data["sender"] = campaign.creator.email
-            if not is_email(validated_data["sender"]):
-                raise exceptions.ValidationError("sender must be a valid email address")
-        else:
-            if not sender:
-                validated_data["sender"] = campaign.creator.phonenumber
-            if not is_phonenumber(validated_data["sender"]):
-                raise exceptions.ValidationError("sender must be a valid phone number")
-        return CampaignMessage.manager.create(dowell_api_key=dowell_api_key, **validated_data)
+    #     validated_data["type"] = campaign.broadcast_type
+    #     validated_data["campaign_id"] = campaign.pkey
+    #     # print("validated data is", validated_data)
+    #     sender = validated_data.get("sender", None)
+    #     if campaign.broadcast_type.lower() == "email":
+    #         if not sender:
+    #             validated_data["sender"] = campaign.creator.email
+    #         if not is_email(validated_data["sender"]):
+    #             raise exceptions.ValidationError("sender must be a valid email address")
+    #     else:
+    #         if not sender:
+    #             validated_data["sender"] = campaign.creator.phonenumber
+    #         if not is_phonenumber(validated_data["sender"]):
+    #             raise exceptions.ValidationError("sender must be a valid phone number")
+    #     return CampaignMessage.manager.create(dowell_api_key=dowell_api_key, **validated_data)
     
 
-    def update(self, campaign_message: CampaignMessage, validated_data):
-        dowell_api_key = self.context.get("dowell_api_key", None)
-        sender = validated_data.get("sender", None)
+    # def update(self, campaign_message: CampaignMessage, validated_data):
+    #     dowell_api_key = self.context.get("dowell_api_key", None)
+    #     sender = validated_data.get("sender", None)
 
-        if sender is not None:
-            if campaign_message.type.lower() == "email":
-                if not is_email(sender):
-                    raise exceptions.ValidationError("sender must be a valid email address")
-            else:
-                if not is_phonenumber(sender):
-                    raise exceptions.ValidationError("sender must be a valid phone number")
-        for attr, value in validated_data.items():
-            setattr(campaign_message, attr, value)
-        campaign_message.is_default = False
-        campaign_message.save(dowell_api_key=dowell_api_key)
-        return campaign_message
+    #     if sender is not None:
+    #         if campaign_message.type.lower() == "email":
+    #             if not is_email(sender):
+    #                 raise exceptions.ValidationError("sender must be a valid email address")
+    #         else:
+    #             if not is_phonenumber(sender):
+    #                 raise exceptions.ValidationError("sender must be a valid phone number")
+    #     for attr, value in validated_data.items():
+    #         setattr(campaign_message, attr, value)
+    #     campaign_message.is_default = False
+    #     campaign_message.save(dowell_api_key=dowell_api_key)
+    #     return campaign_message
 
