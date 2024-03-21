a884427e23d033a9c175f0c3b22158d9f39b4d4a
paolo <paolosiroko@gmail.com>
2024-03-21 16:14:35 +0300

    url fix

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 
 from . import views, webhooks
+from . import _views
 
 app_name = "campaigns"
 
 urlpatterns = [
     path("", views.campaign_list_create_api_view, name="campaign-list-create"),
     path("<str:campaign_id>/", views.campaign_retreive_update_delete_api_view, name="campaign-retrieve-update-delete"),
-    path("user-registration", views.user_registration_view, name="user_registration_view"),
+    path("user-registration", _views.user_registration_view, name="user_registration_view"),
     path("test-email", views.test_email_view, name="test-email"),
     path("<str:campaign_id>/activate-deactivate/", views.campaign_activate_deactivate_api_view, name="campaign-activate-deactivate"),
     path("<str:campaign_id>/message/", views.campaign_message_create_retrieve_api_view, name="campaign-message-create-retreive"),
